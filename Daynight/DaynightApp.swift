602588f73a57dbccff0615119589479c8b017060
Mushfiq Humayoon <mushfiqhumayoon@gmail.com>
2023-11-22 12:58:11 +0400

    Initial Commit

---
+++ Daynight/DaynightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaynightApp.swift
+//  Daynight
+//
+//  Created by Mushfiq Humayoon on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DaynightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
